0084fedca4f058197e841336ad8badd2dd9de46d
Charles Lee <charleslee1904@outlook.com>
2026-01-02 00:28:42 -0800

    added multiple gradient styles

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color(red:0, green:1, blue:0)
-            Color.red
-                .frame(minWidth:200,maxWidth:.infinity, maxHeight:200)
-            Text("Your content")
-                .background(.red)
+        ZStack {
+            VStack(spacing:0) {
+                Color.red
+                Color.blue
+            }
+            
+            
+            Text("Your Content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-      
+        
+        Text("Your content 2")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.red.gradient)
+        
+                    //Angular Gradient
+//        AngularGradient(colors: [
+//            .red,
+//            .yellow,
+//            .green,
+//            .blue,
+//            .purple,
+//            .red
+//        ], center: .center
+//        )
+        
+                    //Radial Gradient
+//        RadialGradient(colors:[.blue,.black],center:.center,
+//                       startRadius:20, endRadius:200)
+//        
+                    //Linear Gradient
+//        LinearGradient(stops: [
+//
+//            .init(color:.white, location: 0.45),
+//            .init(color:.black, location: 0.55),
+//        ],startPoint: .top, endPoint:.bottom)
+
+        .ignoresSafeArea()
         .padding()
     }
+      
 }
 
 #Preview {
