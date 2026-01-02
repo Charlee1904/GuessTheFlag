2df9a25b8b9790702ee0f033fe1cdf25e9bfaf00
Charles Lee <charleslee1904@outlook.com>
2026-01-01 20:03:01 -0800

    Initial Commit

---
+++ Guesstheflag/GuesstheflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesstheflagApp.swift
+//  Guesstheflag
+//
+//  Created by Charles Lee on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GuesstheflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
