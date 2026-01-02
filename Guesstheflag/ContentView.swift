49d5520bd1e6761875033f98f25018d3627a8a70
Charles Lee <charleslee1904@outlook.com>
2026-01-01 21:52:32 -0800

    added some color

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -9,18 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:20){
-            HStack{
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            HStack{
-                Text("4")
-                Text("5")
-                Text("6")
-            }
+        ZStack{
+            Color(red:0, green:1, blue:0)
+            Color.red
+                .frame(minWidth:200,maxWidth:.infinity, maxHeight:200)
+            Text("Your content")
+                .background(.red)
         }
+      
         .padding()
     }
 }
