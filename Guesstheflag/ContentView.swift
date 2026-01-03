fc8afe2e14182a75e2c89331cddaeb9e837bb333
Charles Lee <charleslee1904@outlook.com>
2026-01-02 21:56:54 -0800

    added some button styles

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete selection", action:executeDelete)
+        //        Button("Delete selection", role:.destructive,action:executeDelete)
+        //        }
+        //
+        //    func executeDelete() {
+        //        print("Now deleting..")
+        //    }
+        
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive){}
+                .buttonStyle(.borderedProminent)
+            
         }
-    
-    func executeDelete() {
-        print("Now deleting..")
+        
     }
-    
 }
 #Preview {
     ContentView()
