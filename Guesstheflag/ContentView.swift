1b1ff0effd25f120d5a9bec54744d11510100075
Charles Lee <charleslee1904@outlook.com>
2026-01-02 21:23:36 -0800

    removed some gradients and added a delete button

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -9,52 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing:0) {
-                Color.red
-                Color.blue
-            }
-            
-            
-            Text("Your Content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
+        Button("Delete selection", action:executeDelete)
         }
-        
-        Text("Your content 2")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundStyle(.white)
-            .background(.red.gradient)
-        
-                    //Angular Gradient
-//        AngularGradient(colors: [
-//            .red,
-//            .yellow,
-//            .green,
-//            .blue,
-//            .purple,
-//            .red
-//        ], center: .center
-//        )
-        
-                    //Radial Gradient
-//        RadialGradient(colors:[.blue,.black],center:.center,
-//                       startRadius:20, endRadius:200)
-//        
-                    //Linear Gradient
-//        LinearGradient(stops: [
-//
-//            .init(color:.white, location: 0.45),
-//            .init(color:.black, location: 0.55),
-//        ],startPoint: .top, endPoint:.bottom)
-
-        .ignoresSafeArea()
-        .padding()
+    
+    func executeDelete() {
+        print("Now deleting..")
     }
-      
+    
 }
-
 #Preview {
     ContentView()
 }
