42a5d214212a796a38c926781a36da93428ffe6d
Charles Lee <charleslee1904@outlook.com>
2026-01-04 08:42:16 -0800

    added alerts

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -8,22 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
-        //        Button("Delete selection", role:.destructive,action:executeDelete)
-        //        }
-        //
-        //    func executeDelete() {
-        //        print("Now deleting..")
-        //    }
         
-        VStack {
-            Button("Edit", systemImage:"pencil") {
-                print("button was tapped")
-                
-            } 
-            
+        Button("Show Alert") {
+            showingAlert = true
         }
-        
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK", role:.cancel){}
+        } message: {
+            Text("Please read this.")
+        }
     }
 }
 #Preview {
